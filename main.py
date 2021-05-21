e7b7207b1c3925e36db26a707408d06021829ade
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2021-05-22 01:30:04 +0200

    added better score system

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from mongo_data.Models.score import Score
 from mongo_data.Models.message import Message
 from mongo_data.Models.guilds import Guild
 from secrets.secrets import TOKEN
@@ -17,40 +18,35 @@ leaderboard_anouncement_cooldown_seconds = 3600
 nice_count = dict()
 last_score_anouncment = time.time()-leaderboard_anouncement_cooldown_seconds
 
-bot = commands.Bot(command_prefix='!nice ', description=description, activity=discord.Game("!nice help"))
+bot = commands.Bot(command_prefix='!nice ',
+                   description=description, activity=discord.Game("!nice help"))
 
-def create_regex_pattern(word_list):
-    return f'({"|".join(word_list)})'
-
-def get_amount_of_matches(message):
-    pattern = create_regex_pattern(word_list)
-    return len(re.findall(pattern, message))
-
-def get_score(guild_id):
-    total = Guild(str(guild_id)).get_total().items()
-    nice_count_view = [ (v,k) for k,v in total]
-    nice_count_view.sort(reverse=True)
-    return nice_count_view
 
 def score_message(guild_id):
     msg = 'NICE LEADERBOARD'
-    for i, count in enumerate(get_score(guild_id)):
+    for i, count in enumerate(Score(guild_id).get()):
         v, k = count
         msg += f'\n\t{1 + i}. {k}: {v}'
     return msg
 
+
 @bot.event
 async def on_message(message):
-    Message(message).post()
+    if message.author.id != bot.user.id and not message.content.startswith("!nice"):
+        Message(message).post()
+        return
     await bot.process_commands(message)
+
 
 @bot.event
 async def on_message_delete(message):
     Message(message).delete()
 
+
 @bot.event
 async def on_message_edit(before, after):
     Message(after).update()
+
 
 @bot.event
 async def on_ready():
@@ -58,14 +54,17 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    
+
+
 @bot.command(description="Get the leaderboard")
 async def score(ctx):
     await ctx.send(score_message(ctx.guild.id))
 
+
 @bot.command()
 async def wordlist(ctx):
     await ctx.send(f"Words im looking for: {' '.join(word_list)}")
+
 
 @bot.command(brief='Dont use plz, i give cookies', description='This is only a testing function that the one and only master dev stroid that should use :)')
 async def test(ctx):
@@ -74,6 +73,7 @@ async def test(ctx):
 
 def main():
     bot.run(TOKEN)
+
 
 if __name__ == "__main__":
     main()
