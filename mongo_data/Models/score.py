5b1cc98e1de68b3c8cbbbec33af9dd56c201088a
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2021-05-22 10:59:52 +0200

    Added daily and weekly leaderboard

--- mongo_data/Models/score.py
+++ mongo_data/Models/score.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import re
 from mongo_data.document_base import Document, db
 
 
@@ -5,23 +7,24 @@ class Score(Document):
     def __init__(self, guild_id) -> None:
         self.collection = db[str(guild_id)]
 
-    def get_by_date(self, start, end):
-        return self.collection.find({'timestamp': {'$lt': end, '$gte': start}})
+   
 
     def find(self, **kwargs):
         return [self(**item) for item in self.collection.find(kwargs)]
 
-    def sort_score(self, scores):
-        nice_count_view = [(v, k) for k, v in scores.items()]
-        nice_count_view.sort(reverse=True)
-        return nice_count_view
+    def reduce(self, scores):
+        result = defaultdict(int)
+        for item in scores:
+            result[item["name"]] += item["matches"]
+        return [(v, k) for k, v in result.items()]
 
-    def get(self, span=None):
-        if not span:
-            result = dict()
-            for item in self.collection.find():
-                try:
-                    result[item["name"]] += item["matches"]
-                except KeyError:
-                    result[item["name"]] = item["matches"]
-            return self.sort_score(result)
+    def get(self):
+        scores = self.reduce(self.collection.find())
+        scores.sort(reverse=True)
+        return scores
+
+    def get_by_date(self, start, end):
+        scores = self.collection.find({'timestamp': {'$lt': end, '$gte': start}})
+        scores = self.reduce(scores)
+        scores.sort(reverse=True)
+        return scores
